03f121a600f843faf74c4d00859882bd444f45c8
pusisi1 <pusisi1@naver.com>
2020-08-11 16:43:52 +0900

    settings->allowed

--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vl-1z@_+b#@&c(&%5f8bv5v(29u2d5(wwz5o2bdslk420&994g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com','localhost','127.0.0.1']
 
 
 # Application definition
